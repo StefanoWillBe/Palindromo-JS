14012d76e15b141580e3dce18a83744a779ac1c3
Stefano Russo <consigliererusso@gmail.com>
2023-01-24 13:09:57 +0100

    Palindromo

---
+++ script.js
@@ -0,0 +1,36 @@
+// ESERCITAZIONE
+//● Una funzione per capire se la parola è palindroma
+
+// Chiedo all'utente di darmi una parola e la salvo in una variabile
+
+var parolaUtente = prompt( " Scrivimi una parola in modo che possa valutare se è palindroma o meno" );
+
+var risultato = valutaPalindromo(parolaUtente);
+
+console.log(risultato);
+
+// funzione che accetta un argomento stringa, confronta l'argomento passato e ritorna un buleono
+
+function valutaPalindromo (paroladavalutare) {
+
+    var parolaValutata = false; 
+
+    var parola1 = paroladavalutare.split("");
+
+    parola1.reverse();
+
+    var nuovaStringa = parola1.join("");
+
+    if( paroladavalutare === nuovaStringa ) {
+
+        parolaValutata = true;
+    } 
+
+    return parolaValutata;
+}
+
+
+
+
+
+
